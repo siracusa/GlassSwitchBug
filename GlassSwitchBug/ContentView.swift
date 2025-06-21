15c36c2359d9427f4ee782f90672445c67a715f4
John Siracusa <siracusa@gmail.com>
2025-06-21 09:53:54 -0400

    Initial commit.

--- GlassSwitchBug/ContentView.swift
+++ GlassSwitchBug/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tinted = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+
+            Text(tinted ? "Tinted" : "Not Tinted")
+                .font(.headline)
+
+            Button("Toggle Tint") {
+                tinted.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.extraLarge)
+
+            Spacer()
         }
-        .padding()
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity
+        )
+        .background(
+            Rectangle()
+                .fill(.clear)
+                // BUG EXPLANATION
+                //
+                // After the tinted boolean is toggled to true once, this
+                // glass effect will ALWAYS appear tinted, even when the
+                // tinted boolean is set back to false.
+                .glassEffect(tinted ? .regular.tint(.teal) : .regular, in: .containerRelative)
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .top
+                )
+                .padding()
+        )
     }
 }
 
