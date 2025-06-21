0076bc98fb36b734ae243dbeac7126f6620e1c5e
John Siracusa <siracusa@gmail.com>
2025-06-21 09:28:01 -0400

    Initial Commit

---
+++ GlassSwitchBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlassSwitchBug
+//
+//  Created by Developer on 6/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlassSwitchBug/GlassSwitchBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassSwitchBugApp.swift
+//  GlassSwitchBug
+//
+//  Created by Developer on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassSwitchBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
